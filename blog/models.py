edf582a23a8e4cd0f9e099fa996781c26519266f
Helen Proudman <helen.proudman@students.codeinstitute.net>
2024-09-23 11:32:30 +0000

    post model updated and comment model added and registered with admin.py

--- blog/models.py
+++ blog/models.py
@@ -5,7 +5,7 @@ STATUS = ((0, "Draft"), (1, "Published"))
 
 # Create your models here.
 
-
+# Post model
 class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
@@ -13,6 +13,14 @@ class Post(models.Model):
     content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
-    excerpt = models.TextField
-    updated_on = models.DateTimeField(auto_now_add=True)
-    
+    excerpt = models.TextField(blank=True)
+    updated_on = models.DateTimeField(auto_now=True)
+
+
+# Comment model
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="commenter")
+    body = models.TextField(max_length=200)
+    approved = models.BooleanField(default=False)
+    created_on = models.DateTimeField(auto_now_add=True)    
